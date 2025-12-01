6f4046bcdc522c574b28c1b29836bd117c9e4862
KwangGod <kwanggod00@gmail.com>
2025-12-01 20:40:23 +0900

    chore: comment

--- Assets/02.Scripts/Room/RoomToRoom.cs
+++ Assets/02.Scripts/Room/RoomToRoom.cs
@@ -6,6 +6,10 @@ public class RoomToRoom : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+
+            //다음 방 번호 세팅
+            //몬스터 초기화
+            //이상현상 초기화
             collision.transform.position = new Vector3(25f, 1f);
         }
 
