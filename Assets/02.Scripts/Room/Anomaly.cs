bfcf08c284945010149d5ff47aca35a9401da8fb
KwangGod <kwanggod00@gmail.com>
2025-12-02 02:13:11 +0900

    feat: anomaly
    이상형상 클래스 생성 = 시야, 조명, 오브젝트

---
+++ Assets/02.Scripts/Room/Anomaly.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Anomaly : MonoBehaviour
+{
+    public Light2D playerLight;
+    public Light2D globalLight;
+    public Sprite baseSprite;
+    public Sprite anomalySprite;
+    public GameObject[] objects;
+
+    public bool isAnomaly = false;
+
+
+
+
+
+    private void Start()
+    {
+        ResetAnomaly();// 처음시작시 셋팅
+    }
+
+    public void initAnomaly() //초기화 및 재실행
+    {
+        ResetAnomaly();
+        setAnomaly();
+    }
+
+    private void setAnomaly()
+    {
+        isAnomaly = Random.value < 0.5f; // 50% 확률로 불값 변경
+        if (!isAnomaly)
+        {
+            ResetAnomaly();
+            return;
+        }
+
+        int random = Random.Range(0, 4); // 간단 가중치 = 0~3 중 하나 뽑기 아래는 그 케이스
+        switch (random)
+        {
+            case 0:
+                playerLight.pointLightOuterRadius = 3f;
+                break;
+
+            case 1:
+                globalLight.intensity = 1f;
+                break;
+
+            case 2:
+                globalLight.intensity = 0f;
+                break;
+
+            case 3:
+                objects[Random.Range(0, objects.Length)].GetComponent<SpriteRenderer>().sprite = anomalySprite;//배치된 오브젝트 중 하나의 오브젝트 스프라이트를 이상현상스프라이트로 변경
+                break;
+        }
+
+    }
+    private void ResetAnomaly()//초기화 처음 상태 유지
+    {
+        playerLight.pointLightOuterRadius = 10f;
+        globalLight.intensity = 0.3f;
+        foreach (var obj in objects)
+        {
+            obj.GetComponent<SpriteRenderer>().sprite = baseSprite;
+        }
+    }
+}
--- Assets/02.Scripts/Room/RoomToRoom.cs
+++ Assets/02.Scripts/Room/RoomToRoom.cs
@@ -4,24 +4,25 @@ public class RoomToRoom : MonoBehaviour
 {
     [SerializeField] private Collider2D topEntranceCollider;
     [SerializeField] private Collider2D bottomEntranceCollider;
-    bool anomaly; // 나중에 게임매니저나 맵관련에서 이상현상 체크 값 가져오기
+    public Anomaly anomaly;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (!anomaly && collision.IsTouching(topEntranceCollider) || anomaly && collision.IsTouching(bottomEntranceCollider))//이상현상 없고 위로, 이상현상 있고 아래로 [정답처리]
+            if (!anomaly.isAnomaly && collision.IsTouching(topEntranceCollider) || anomaly.isAnomaly && collision.IsTouching(bottomEntranceCollider))//이상현상 없고 위로, 이상현상 있고 아래로 [정답처리]
             {
                 //방번호 ++
+                Debug.Log("정답");//체크완
             }
             else //[오답 처리]
             {
                 //방번호 초기화
+                Debug.Log("오답");//체크완
             }
             //몬스터 초기화
-            //이상현상 초기화
+            anomaly.initAnomaly();//리셋 후 초기화
             collision.transform.position = new Vector3(25f, 1f);
         }
-
     }
 }
