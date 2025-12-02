eb118ae55f019d2ac5818945537a4805b0a84468
UihwanLee <luh5063014@naver.com>
2025-12-02 12:17:54 +0900

    feat: ItemSO 클래스 구현

---
+++ Assets/04.ScriptableObject/Item/ItemSO.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectTargetType
+{
+    Player,
+    Advisor,
+}
+
+[System.Serializable]
+public class EffectInfo
+{
+    [field: Header("이름")]
+    [field: SerializeField] public string Name { get; private set; }
+    [field: Header("설명")]
+    [field: SerializeField] public string Desc { get; private set; }
+    [field: Header("효과 - Key")]
+    [field: SerializeField] public string EN { get; private set; }
+    [field: Header("효과 - Value")]
+    [field: SerializeField] public string Value { get; private set; }
+    [field: Header("적용 대상")]
+    [field: SerializeField] public EffectTargetType TargetType { get; private set; }
+}
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Data/Item")]
+public class ItemSO : ScriptableObject
+{
+    [field: Header("ID")]
+    [field: SerializeField] public int ID { get; private set; }
+    [field: SerializeField] public EffectInfo EffectInfo { get; private set; }
+}
