8297c784ba170f3223c88e75f48121a7d79d6661
UihwanLee <luh5063014@naver.com>
2025-12-02 14:36:48 +0900

    feat: 데미지/경험치/골드 흭득 텍스트를 표시할 FloatingText 구현
    - FloatingText 구현
    - FloatingTextPool 구현
    - FloatingTextPoolManager 구현

---
+++ Assets/02.Scripts/FloatingText/FloatingText.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI floatingText;
+
+    private Coroutine _fadeCoroutine;
+    private WaitForSeconds waitForSeconds;
+    private float _fadeDuration;
+
+    private FloatingTextPoolManager floatingTextPoolManager;
+
+    private void Awake()
+    {
+        floatingText = GetComponent<TextMeshProUGUI>();
+
+        waitForSeconds = new WaitForSeconds(0.1f);
+
+        Initialize();
+    }
+
+    private void Start()
+    {
+        floatingTextPoolManager = FloatingTextPoolManager.Instance;
+    }
+
+    public void Initialize()
+    {
+        // 색상 초기화
+        Color color = floatingText.color;
+        color.a = 1.0f;
+        floatingText.color = color;
+
+        // Text 초기화
+        floatingText.text = string.Empty;
+    }
+
+    public void SetText(string text)
+    {
+        this.floatingText.text = text;
+    }
+
+    public void SetColor(Color color)
+    {
+        this.floatingText.color = color;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        this.transform.position = position;
+    }
+
+    public void SetDuration(float duration)
+    {
+        _fadeDuration = duration;
+        waitForSeconds = new WaitForSeconds(_fadeDuration);
+    }
+
+    public void StartFadeCoroutine(FloatingTextSO data)
+    {
+        if (_fadeCoroutine != null) StopCoroutine(_fadeCoroutine);
+        _fadeCoroutine = StartCoroutine(FadeCoroutine(data));
+    }
+
+    public IEnumerator FadeCoroutine(FloatingTextSO data)
+    {
+        float fadeTime = _fadeDuration;
+        float curTime = 0.0f;
+        Color tempColor = floatingText.color;
+        Vector3 targetPosition = Vector3.zero;
+        float alpha = 0;
+
+        while (curTime < fadeTime)
+        {
+            curTime += Time.deltaTime;
+
+            float t = (curTime / fadeTime);
+
+            // alpha 값 수정
+            alpha = 1f - t;
+            tempColor.a = alpha;
+            floatingText.color = tempColor;
+
+            // 위치 값 수정
+            targetPosition = new Vector3(transform.position.x, transform.position.y + data.FloaingDist, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, t);
+
+            yield return null;
+        }
+
+        tempColor.a = 0;
+        floatingText.color = tempColor;
+        transform.position = targetPosition;
+
+        // 반환
+        floatingTextPoolManager.Release(data.Name, this.gameObject);
+    }
+}
