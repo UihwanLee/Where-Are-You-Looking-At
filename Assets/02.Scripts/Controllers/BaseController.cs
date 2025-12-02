209cef6c46796b1eb410235cda50f594eadbbdb4
bbbp98 <qjarms1300@gmail.com>
2025-12-02 09:09:37 +0900

    feat: 키 입력을 통한 플레이어 움직임

---
+++ Assets/02.Scripts/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    protected Vector2 moveDirection = Vector2.zero;
+    public Vector2 MoveDirection { get { return moveDirection; } }
+
+    protected float moveSpeed = 5f;
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Flip();
+    }
+
+    private void Movement()
+    {
+        Vector3 direction = transform.right * moveDirection.x + transform.up * moveDirection.y;
+        Vector3 moveTransform = direction.normalized * moveSpeed * Time.deltaTime;
+        transform.position += moveTransform;
+    }
+
+    private void Flip()
+    {
+        if (moveDirection.x < 0)
+            spriteRenderer.flipX = true;
+        else if (moveDirection.x > 0)
+            spriteRenderer.flipX = false;
+    }
+}
---
+++ Assets/02.Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            moveDirection = context.ReadValue<Vector2>();
+        else if (context.phase == InputActionPhase.Canceled)
+            moveDirection = Vector2.zero;
+    }
+}
