f538b669b19c5ca8df86376be6a587aee0a5eb69
KwangGod <kwanggod00@gmail.com>
2025-12-02 17:03:11 +0900

    fix: roomPrefabs / anomaly
    프리팹연결수정 및 이상현상 변경

--- Assets/02.Scripts/05.Room/RoomToRoom.cs
+++ Assets/02.Scripts/05.Room/RoomToRoom.cs
@@ -5,19 +5,19 @@ public class RoomToRoom : MonoBehaviour
     [SerializeField] private Collider2D topEntranceCollider;
     [SerializeField] private Collider2D bottomEntranceCollider;
     public Anomaly anomaly;
-
+    public int roomNumber;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             if (!anomaly.isAnomaly && collision.IsTouching(topEntranceCollider) || anomaly.isAnomaly && collision.IsTouching(bottomEntranceCollider))//이상현상 없고 위로, 이상현상 있고 아래로 [정답처리]
             {
-                //방번호 ++
+                roomNumber++;
                 Debug.Log("정답");//체크완
             }
             else //[오답 처리]
             {
-                //방번호 초기화
+                roomNumber = 0;
                 Debug.Log("오답");//체크완
             }
             //몬스터 초기화
