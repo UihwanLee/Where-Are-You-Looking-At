af78091a5dc3eb711af85d90c250fbcc7019f41a
UihwanLee <luh5063014@naver.com>
2025-12-02 10:44:42 +0900

    feat: Stat 클래스 구현
    - AdvisorStat 클래스 구현
    - MonsterStat 클래스 구현
    - PlayerStat 클래스 구현

---
+++ Assets/02.Scripts/Stat/AdvisorStat.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AdvisorStat : MonoBehaviour
+{
+    [SerializeField] private AdvisorStatSO data;
+    
+    [SerializeField] private Attribute atk;                             // 공격력
+    [SerializeField] private Attribute attackSpeed;                     // 공격 속도 
+    [SerializeField] private Attribute attackRange;                     // 공격 범위
+    [SerializeField] private Attribute critChance;                      // 치명타 확률
+    [SerializeField] private Attribute critMultiplier;                  // 치명타 계수
+    [SerializeField] private Attribute projectileSpeed;                 // 투사체 속도
+
+    #region 파일 로드
+
+    private const string FileName = "advisor_data.json";
+
+    /// <summary>
+    /// 데이터를 Json 파일로 저장.
+    /// </summary>
+    public void SaveData()
+    {
+        string json = JsonUtility.ToJson(this);
+        string filePath = Path.Combine(Application.persistentDataPath, FileName);
+
+        try
+        {
+            File.WriteAllText(filePath, json);
+            Debug.Log($"데이터 저장 성공: {filePath}");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"데이터 저장 실패: {e.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Json 파일에서 데이터를 불러오기.
+    /// </summary>
+    public PlayerStat LoadData()
+    {
+        string filePath = Path.Combine(Application.persistentDataPath, FileName);
+
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+
+                PlayerStat loadedData = JsonUtility.FromJson<PlayerStat>(json);
+                Debug.Log($"데이터 로드 성공: {filePath}");
+
+                return loadedData;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"데이터 로드 실패: {e.Message}. ");
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("저장된 파일이 없습니다.");
+            // 저장된 파일이 없으면 초기값으로 시작
+            return null;
+        }
+    }
+
+    #endregion 
+
+    private Dictionary<AttributeType, Attribute> attributeDict;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        atk = new Attribute(data.Atk);
+        attackSpeed = new Attribute(data.AttackSpeed);
+        attackRange = new Attribute(data.AttackRange);
+        critChance = new Attribute(data.CritChance);
+        critMultiplier = new Attribute(data.CritMultiplier);
+        projectileSpeed = new Attribute(data.ProjectileSpeed);
+
+        attributeDict = new Dictionary<AttributeType, Attribute>();
+        attributeDict.Add(AttributeType.Atk, atk);
+        attributeDict.Add(AttributeType.AttackSpeed, attackSpeed);
+        attributeDict.Add(AttributeType.AttackRange, attackRange);
+        attributeDict.Add(AttributeType.CritChance, critChance);
+        attributeDict.Add(AttributeType.CritMultiplier, critMultiplier);
+        attributeDict.Add(AttributeType.ProjectileSpeed, projectileSpeed);
+    }
+
+    public void Add(AttributeType type, float amount)
+    {
+        if (attributeDict.TryGetValue(type, out Attribute attribute))
+        {
+            attribute.AddValue(amount);
+        }
+    }
+
+    public void Sub(AttributeType type, float amount)
+    {
+        if (attributeDict.TryGetValue(type, out Attribute attribute))
+        {
+            attribute.SubValue(amount);
+        }
+    }
+
+    public void Set(AttributeType type, float amount)
+    {
+        if (attributeDict.TryGetValue(type, out Attribute attribute))
+        {
+            attribute.SetValue(amount);
+        }
+    }
+
+    #region 프로퍼티
+
+    public float Atk { get { return atk.Value; } }
+    public float AttackSpeed { get { return attackSpeed.Value; } }
+    public float AttackRange { get { return attackRange.Value; } }
+    public float CritChance { get { return critChance.Value; } }
+    public float CritMultiplier { get { return critMultiplier.Value; } }
+    public float ProjectileSpeed { get {return projectileSpeed.Value; } }
+
+    #endregion
+}
---
+++ Assets/02.Scripts/Stat/PlayerStat.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using static Cinemachine.DocumentationSortingAttribute;
+
+public class PlayerStat : MonoBehaviour
+{
+    [SerializeField] private PlayerStatSO data;
+
+    [SerializeField] private Attribute maxHp;                        // 최대 체력
+    [SerializeField] private Attribute speed;                        // 속도
+    [SerializeField] private Attribute reproductionHp;               // 체력 재생
+    [SerializeField] private Attribute recoveryHp;                   // 체력 회복
+    [SerializeField] private Attribute defense;                      // 방어
+    [SerializeField] private Attribute evasion;                      // 회피
+    [SerializeField] private Attribute luck;                         // 행운
+    [SerializeField] private Attribute absorptionItem;               // 아이템 흡수
+
+    #region 파일 로드
+
+    private const string FileName = "player_data.json";
+
+    /// <summary>
+    /// 데이터를 Json 파일로 저장.
+    /// </summary>
+    public void SaveData()
+    {
+        string json = JsonUtility.ToJson(this);
+        string filePath = Path.Combine(Application.persistentDataPath, FileName);
+
+        try
+        {
+            File.WriteAllText(filePath, json);
+            Debug.Log($"데이터 저장 성공: {filePath}");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"데이터 저장 실패: {e.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Json 파일에서 데이터를 불러오기.
+    /// </summary>
+    public PlayerStat LoadData()
+    {
+        string filePath = Path.Combine(Application.persistentDataPath, FileName);
+
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+
+                PlayerStat loadedData = JsonUtility.FromJson<PlayerStat>(json);
+                Debug.Log($"데이터 로드 성공: {filePath}");
+
+                return loadedData;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"데이터 로드 실패: {e.Message}. ");
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("저장된 파일이 없습니다.");
+            // 저장된 파일이 없으면 초기값으로 시작
+            return null;
+        }
+    }
+
+    #endregion 
+
+    private Dictionary<AttributeType, Attribute> attributeDict;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        maxHp = new Attribute(data.MaxHp);
+        speed = new Attribute(data.Speed);
+        reproductionHp = new Attribute(data.ReproductionHp);
+        recoveryHp = new Attribute(data.RecoveryHp);
+        defense = new Attribute(data.Defense);
+        evasion = new Attribute(data.Evasion);
+        luck = new Attribute(data.Luck);
+        absorptionItem = new Attribute(data.AbsorptionItem);
+
+        attributeDict = new Dictionary<AttributeType, Attribute>();
+        attributeDict.Add(AttributeType.MaxExp, maxHp);
+        attributeDict.Add(AttributeType.Speed, speed);
+        attributeDict.Add(AttributeType.ReproductionHp, reproductionHp);
+        attributeDict.Add(AttributeType.RecoveryHp, recoveryHp);
+        attributeDict.Add(AttributeType.Defense, defense);
+        attributeDict.Add(AttributeType.Evasion, evasion); 
+        attributeDict.Add(AttributeType.Luck, luck);
+        attributeDict.Add(AttributeType.AbsorptionItem, absorptionItem);    
+    }
+
+    public void Add(AttributeType type, float amount)
+    {
+        if (attributeDict.TryGetValue(type, out Attribute attribute))
+        {
+            attribute.AddValue(amount);
+        }
+    }
+
+    public void Sub(AttributeType type, float amount)
+    {
+        if (attributeDict.TryGetValue(type, out Attribute attribute))
+        {
+            attribute.SubValue(amount);
+        }
+    }
+
+    public void Set(AttributeType type, float amount)
+    {
+        if (attributeDict.TryGetValue(type, out Attribute attribute))
+        {
+            attribute.SetValue(amount);
+        }
+    }
+
+    #region 프로퍼티
+
+    public float MaxHp { get { return maxHp.Value; } }
+    public float Speed { get { return speed.Value; } }
+    public float ReproductionHp { get { return  reproductionHp.Value; } }
+    public float RecoveryHp { get { return recoveryHp.Value; } }
+    public float Defense { get { return defense.Value; } }
+    public float Evasion { get { return evasion.Value; } }
+    public float Luck { get { return luck.Value; }}
+    public float AbsorptionItem {  get { return absorptionItem.Value; } }
+
+    #endregion
+}
