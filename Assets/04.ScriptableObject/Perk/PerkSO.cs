bf59fe4dccb9e9bc72cb292e14b18147642c9be3
UihwanLee <luh5063014@naver.com>
2025-12-02 12:32:07 +0900

    chore: Item -> Perk으로 이름 변경

--- Assets/04.ScriptableObject/Perk/PerkSO.cs
+++ Assets/04.ScriptableObject/Perk/PerkSO.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EffectTargetType
+public enum PerkTargetType
 {
     Player,
     Advisor,
 }
 
-[System.Serializable]
-public class EffectInfo
+[CreateAssetMenu(fileName = "NewPerk", menuName = "Data/Perk")]
+public class PerkSO : ScriptableObject
 {
+    [field: Header("ID")]
+    [field: SerializeField] public int ID { get; private set; }
     [field: Header("이름")]
     [field: SerializeField] public string Name { get; private set; }
     [field: Header("설명")]
@@ -22,13 +24,5 @@ public class EffectInfo
     [field: Header("효과 - Value")]
     [field: SerializeField] public string Value { get; private set; }
     [field: Header("적용 대상")]
-    [field: SerializeField] public EffectTargetType TargetType { get; private set; }
-}
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Data/Item")]
-public class ItemSO : ScriptableObject
-{
-    [field: Header("ID")]
-    [field: SerializeField] public int ID { get; private set; }
-    [field: SerializeField] public EffectInfo EffectInfo { get; private set; }
+    [field: SerializeField] public PerkTargetType TargetType { get; private set; }
 }
