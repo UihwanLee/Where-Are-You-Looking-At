cb0007cb0c9bdb35a497ad2a07b0b903a9469b59
UihwanLee <luh5063014@naver.com>
2025-12-03 19:58:26 +0900

    feat: PlayerAdviceHandler를 구현하여 효과를 적용시키게 설계

---
+++ Assets/02.Scripts/01.Player/Handler/PlayerAdviceHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAdviceHandler : MonoBehaviour
+{
+    private Player player;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    public void ApplyAdvice(Advice advice)
+    {
+        switch(advice.Data.EN)
+        {
+            case "EN_001":
+                player.Stat.Add(AttributeType.MaxHp, advice.Data.Value);
+                break;
+            case "EN_002":
+                player.Stat.Add(AttributeType.Speed, advice.Data.Value);
+                break;
+            case "EN_003":
+                player.Stat.Add(AttributeType.ReproductionHp, advice.Data.Value);
+                break;
+            default:
+                break;
+        }
+
+        // Stat UI 갱신
+        ClearWindowManager.Instance.OnPlayerStatChange?.Invoke();
+    }
+}
