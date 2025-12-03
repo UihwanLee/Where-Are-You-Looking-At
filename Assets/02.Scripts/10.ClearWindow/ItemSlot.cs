35317b8b4fbf553aed32417365b33d1eaf18a2fe
UihwanLee <luh5063014@naver.com>
2025-12-03 18:34:04 +0900

    feat: 슬롯 간 아이템 이동이 가능하게 구현

--- Assets/02.Scripts/10.ClearWindow/ItemSlot.cs
+++ Assets/02.Scripts/10.ClearWindow/ItemSlot.cs
@@ -2,29 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    // 상품 슬롯은 8개로 고정
-    private int MAX_PRDOUCT_SLOT = 8;
-
-    // 아이템 슬롯에 넣을 Item 정보
-    [Header("아이템 정보")]
-    [SerializeField] private ISellable item;
-
     [Header("컴포넌트 UI")]
     [SerializeField] private Button btn;
     [SerializeField] private Image highlight;
     [SerializeField] private Image icon;
 
+    // 아이템 슬롯에 넣을 Item 정보
+    [Header("아이템 정보")]
+    public ISellable item;
+
+    private int index;
+
     private bool isPurchase = false;
+    private ISlotable manager;
 
     private void Reset()
     {
         btn = transform.FindChild<Button>("ItemSlot");
         highlight = transform.FindChild<Image>("Highlight");
         icon = transform.FindChild<Image>("ItemIcon");
+    }
+
+    public void SetManager(ISlotable manager)
+    {
+        this.manager = manager;
+    }
+
+    public void SetIndex(int index)
+    {
+        this.index = index;
     }
 
     public void SetButton(Action<ItemSlot> onClickEvent)
@@ -62,6 +74,71 @@ public class ItemSlot : MonoBehaviour
     {
         isPurchase = true;
         ResetSlot();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (item != null && manager.GetSlotTpye() != SlotType.Shop)
+        {
+            // DragSlot 설정
+            DragSlot.instance.dragSlot = this;
+            DragSlot.instance.SetDragItem(this.icon.sprite);
+            DragSlot.instance.transform.position = eventData.position;
+
+            UnClick();
+
+            this.icon.color = new Color(1, 1, 1, 0);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (item != null && manager.GetSlotTpye() != SlotType.Shop)
+        {
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (manager.GetSlotTpye() == SlotType.Shop) return;
+
+        DragSlot.instance.dragSlot.icon.color = new Color(1, 1, 1, 1);
+        this.icon.color = new Color(1, 1, 1, 1);
+
+        DragSlot.instance.HideDragItem();
+        DragSlot.instance.dragSlot = null;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (manager.GetSlotTpye() == SlotType.Shop) return;
+
+        if (DragSlot.instance.dragSlot != null)
+        {
+            ChangeSlot();
+        }
+    }
+
+    private void ChangeSlot()
+    {
+        ItemSlot sourceSlot = DragSlot.instance.dragSlot;
+        ISellable sourceItem = sourceSlot.item;
+
+        ISellable targetItem = this.item;
+
+        this.SetItem(sourceItem);
+
+        manager.OnClickSlot(this);
+
+        if (targetItem != null)
+        {
+            sourceSlot.SetItem(targetItem);
+        }
+        else
+        {
+            sourceSlot.ResetSlot();
+        }
     }
 
     #region 프로퍼티 
