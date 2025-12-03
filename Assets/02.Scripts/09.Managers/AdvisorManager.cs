5ca65e445a4c0a816394843c3d9e9b5bbf7df388
bbbp98 <qjarms1300@gmail.com>
2025-12-03 19:53:40 +0900

    fix: 조언자 수에 따라 조언자의 위치가 바뀌도록 수정

--- Assets/02.Scripts/09.Managers/AdvisorManager.cs
+++ Assets/02.Scripts/09.Managers/AdvisorManager.cs
@@ -9,12 +9,15 @@ public enum AdvisorType
 
 public class AdvisorManager : MonoBehaviour
 {
+    const int MaxAdvisorCount = 6;
+
     [field: SerializeField] private List<GameObject> advisorPrefabs = new List<GameObject>();
 
-    private Vector3[] advisorPivots = new Vector3[6];
-
-    private float radius = 1f;
     private int currentAdvisorIndex = 0;
+    private Vector3[] advisorPivots = new Vector3[MaxAdvisorCount];
+    private float radius = 1f;
+
+    private GameObject[] activeAdvisor = new GameObject[MaxAdvisorCount];
 
     private void Start()
     {
@@ -31,18 +34,28 @@ public class AdvisorManager : MonoBehaviour
 
     public void AddAdvisors()
     {
-        if (currentAdvisorIndex >= advisorPivots.Length) return;
+        if (currentAdvisorIndex >= MaxAdvisorCount) return;
 
+        // 1. 조언자 생성
         GameObject go = Instantiate(advisorPrefabs[0], transform);
-        go.transform.localPosition += advisorPivots[currentAdvisorIndex];
+        activeAdvisor[currentAdvisorIndex] = go;
         currentAdvisorIndex++;
+
+        ArrangementPivots();
+
+        // 2. 조언자 위치 재배치
+        for (int i = 0; i < activeAdvisor.Length; i++)
+        {
+            if (activeAdvisor[i] == null) continue;
+            activeAdvisor[i].transform.localPosition = advisorPivots[i];
+        }
     }
 
     private void ArrangementPivots()
     {
-        float angleStep = 360f / advisorPivots.Length;
+        float angleStep = 360f / currentAdvisorIndex;
 
-        for (int i = 0; i < advisorPivots.Length; i++)
+        for (int i = 0; i < currentAdvisorIndex; i++)
         {
             float angle = angleStep * i;
             float rad = angle * Mathf.Deg2Rad;
